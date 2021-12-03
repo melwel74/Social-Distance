34aacd8a513ff49338eb2f3889cb269a17ffebdb
dianepj12@gmail.com <dianepj12@gmail.com>
2021-12-03 13:54:39 -0600

    making blog post

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
 function blogPost(){
-    
+
 }
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    //get value from input Element
+    var username = nameInputEl.ariaValueMax.trim();
+    
+    if(username){
+        getUserRepos(username);
+        nameInputEl.value ="";
+    }else{
+
+    alert("Please enter a GitHub username");
+    }
+    console.log(event);
+};
+userFormEl.addEventListener("submit", formSubmitHandler);
+// fun thing to add
+// setTimeout(function() {
+//     // called after 5 seconds, regardless if button was clicked
+//     console.log("How is Everything going so Far? Would you like to share how you are feeling?");
+//    }, 5000);
