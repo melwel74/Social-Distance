194f454c81e2c415f9ce028bcbf8b3810e3a1fbd
dianepj12@gmail.com <dianepj12@gmail.com>
2021-12-04 12:50:02 -0600

    making text area is not equal to null

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,12 @@
 
-var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#username");
-function blogPost(){
+ var blogpostText = document.querySelector("#text");
 
+ if(!blogpostText.contains("")){
+     blogpostText.value +=word;
+ }
+function run()
+{
+    var txt=document.getElementById('text').value;
+    alert(txt);
 }
-var formSubmitHandler = function(event){
-    event.preventDefault();
-    //get value from input Element
-    var username = nameInputEl.ariaValueMax.trim();
-    
-    if(username){
-        getUserRepos(username);
-        nameInputEl.value ="";
-    }else{
-
-    alert("Please enter a GitHub username");
-    }
-    console.log(event);
-};
-userFormEl.addEventListener("submit", formSubmitHandler);
-// fun thing to add
-// setTimeout(function() {
-//     // called after 5 seconds, regardless if button was clicked
-//     console.log("How is Everything going so Far? Would you like to share how you are feeling?");
-//    }, 5000);
+    run()
