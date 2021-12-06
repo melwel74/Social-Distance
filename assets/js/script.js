fb483d109a31e0ec01376544d3bcf0dbc7fca158
Cristian Trigueros <cristiantrigueros@Cristians-MBP.lan>
2021-12-05 20:11:11 -0600

    updated css/html

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var blogpostText = document.querySelector("#text");
+
+if (!blogpostText.contains("")) {
+  blogpostText.value += word;
+}
+function run() {
+  var txt = document.getElementById("text").value;
+  alert(txt);
+}
+run();
