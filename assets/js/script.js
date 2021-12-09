c3516d179f13e76aae341b8d432fd8ed959c8003
dianepj12@gmail.com <dianepj12@gmail.com>
2021-12-09 14:38:07 -0600

    dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,7 @@ const bannerImage = document.querySelector('.title');
 const banner = document.querySelector(".banner");
 let bannerPath;
 
-const pulishBtn = document.querySelector('.publish-btn');
+const publishBtn = document.querySelector('.publish-btn');
 const uploadInput = document.querySelector('#image-upload');
 
 bannerImage.addEventListener('change', ()=>{
@@ -64,6 +64,8 @@ const uploadImage = (uploadFile, uploadType) =>{
       bannerPath = '${location.origin}/${data}';
       banner.style.backgroundImage = 'url("${bannerPath}")';
    }) 
+  }else{
+      alert("upload Image only");
   }
 }
 
@@ -73,18 +75,49 @@ const addImage = (imagepath,alt)=>{
     articleField.value =articleField.value.slice(0,curPos) +textToInsert +
      articleField.value.slice()
 }
+// eDjs
+let months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
 
 
+//eD.js
+publishBtn.addEventListener('click',() =>{
+    if(articleField.value.length && blogTitleField.value.length){
+        //generating id
+        let letters ='abcdefghijklmnoqrstuvwxyz';
+        let blogTitle = blogTitleField.value.split(" ").join("-");
+        let id = '';
+        for(let i = 0; i < 4;i++){
+            id +=letters[Math.floor(Math.random()*letters.length)];
+        }
+        //setting up docName
+        let docName = '${blogTitle}-${id}';
+        let date = new Date(); //for published at info
 
-
-
-
-// Check to see if the blog is empty
- var blogpostText = document.querySelector("#text");
- 
-if (document.getElementById("#text").value == null)
-{
-    alert("There is nothing in the text area")
-    document.getElementById("field").style.display ="none";
-
+        // acess firstore with db variable;
+        db.collection("blogs").doc(docName).set({
+            title:blogTitleField.value,
+            article:articleField.value,
+            bannerImage:bannerpath,
+            publishedAt:'${date.getDate()} ${months[date.getMonth()]} ${date.get.getFullYear()}'
+        })
+        .then(()=> {
+                console.log('date entered');
+        })
+        .catch((err)=>{
+            console.error(err);
+        })
+    }
 }
+
+
+
+
+// // Check to see if the blog is empty
+//  var blogpostText = document.querySelector("#text");
+ 
+// if (document.getElementById("#text").value == null)
+// {
+//     alert("There is nothing in the text area")
+//     document.getElementById("field").style.display ="none";
+
+// }
