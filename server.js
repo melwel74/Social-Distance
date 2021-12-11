9259df2839837213493b2ffc9cd7ba63305213aa
dianepj12@gmail.com <dianepj12@gmail.com>
2021-12-11 11:18:48 -0600

    post

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-const path = require('path')
+const path = require('path');
 
 //for images if time
 // const fileupload = require('express-fileupload');
 
 
-const express = require('express')
+const express = require('express');
+// import { create } from 'express-handlebars';
+// const hbs = create({});
+// import { join } from 'path';
 
+// //for images if time
+// // const fileupload = require('express-fileupload');
+
+
+// import express, { static, json, urlencoded } from 'express';
 //Instantiate the server
 const app = express();
 app.engine('handlebars', hbs.engine);
@@ -15,6 +23,8 @@ app.set('view engine', 'handlebars');
 app.use(express.static(path.join (__dirname,'public')))//initial_path(let)
 app.use(express.json ())
 app.use(express.urlencoded({extended:false}))
+
+
 
 //new express route
 app.get('/api/social-distance', (req,res) =>{
@@ -25,12 +35,10 @@ app.use(require('./controllers'))
 
 //make the server listen
 app.listen(3001,() =>{
-    console.log('hello yall!')
-}); 
-//connection with front end -Dp
-app.use((req,res)=>{
-    res.json("404");
-})
-app.listen("3000",()=>{
     console.log('listening.....');
+})
+
+//-dp
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(initial_path,"index.html"));
 })
