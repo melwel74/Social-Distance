c98bc2481e54f9d1ecb480a876987e25441c9f9b
dianepj12@gmail.com <dianepj12@gmail.com>
2021-12-14 18:42:34 -0600

    add display

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-
-const express = require('express')
+// added Fs-D
+let initial_path = path.join(__dirname,"public");
+// const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({});
 
 //Instantiate the server
 const app = express();
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.use(express.static(path.join (__dirname,'public')))
-app.use(express.json ())
-app.use(express.urlencoded({extended:false}))
+// added Fs-D
+app.use(express.static(initial_path));
+
+app.get('/',(req,res) =>{
+    res.sendFile(path.join(initial_path, "index.html"));
+})
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+// app.use(express.static(path.join (__dirname,'public')))
+// app.use(express.json ())
+// app.use(express.urlencoded({extended:false}))
 
 //new express route
 app.get('/api/social-distance', (req,res) =>{
@@ -19,8 +26,103 @@ app.get('/api/social-distance', (req,res) =>{
 })
 app.use(require('./controllers'))
 
+//upload file 
+let date =new Date();
 
 //make the server listen
-app.listen(3001,() =>{
-    console.log('hello yall!')
+app.listen('3000', () => {
+    console.log('listening....')
 });
+// if (process.env.NODE_ENV !== "production") {
+//     require("dotenv").config();
+//   }
+  
+//   const express = require("express");
+//   const app = express();
+//   const bcrypt = require("bcrypt");
+//   const passport = require("passport");
+//   const flash = require("express-flash");
+//   const session = require("express-session");
+//   const methodOverride = require("method-override");
+  
+//   const initializePassport = require("./passport-config");
+//   initializePassport(
+//     passport,
+//     (email) => users.find((user) => user.email === email),
+//     (id) => users.find((user) => user.id === id)
+//   );
+  
+//   const users = [];
+  
+//   app.set("view-engine", "ejs");
+//   app.use(express.urlencoded({ extended: false }));
+//   app.use(flash());
+//   app.use(
+//     session({
+//       secret: process.env.SESSION_SECRET,
+//       resave: false,
+//       saveUninitialized: false,
+//     })
+//   );
+//   app.use(passport.initialize());
+//   app.use(passport.session());
+//   app.use(methodOverride("_method"));
+  
+//   app.get("/", checkAuthenticated, (req, res) => {
+//     res.render("index.ejs", { name: req.user.name });
+//   });
+  
+//   app.get("/login", checkNotAuthenticated, (req, res) => {
+//     res.render("login.ejs");
+//   });
+  
+//   app.post(
+//     "/login",
+//     checkNotAuthenticated,
+//     passport.authenticate("local", {
+//       successRedirect: "/",
+//       failureRedirect: "/login",
+//       failureFlash: true,
+//     })
+//   );
+  
+//   app.get("/register", checkNotAuthenticated, (req, res) => {
+//     res.render("register.ejs");
+//   });
+  
+//   app.post("/register", checkNotAuthenticated, async (req, res) => {
+//     try {
+//       const hashedPassword = await bcrypt.hash(req.body.password, 10);
+//       users.push({
+//         id: Date.now().toString(),
+//         name: req.body.name,
+//         email: req.body.email,
+//         password: hashedPassword,
+//       });
+//       res.redirect("/login");
+//     } catch {
+//       res.redirect("/register");
+//     }
+//   });
+  
+//   app.delete("/logout", (req, res) => {
+//     req.logOut();
+//     res.redirect("/login");
+//   });
+  
+//   function checkAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) {
+//       return next();
+//     }
+  
+//     res.redirect("/login");
+//   }
+  
+//   function checkNotAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) {
+//       return res.redirect("/");
+//     }
+//     next();
+//   }
+  
+//   app.listen(3000);
