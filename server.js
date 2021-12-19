f77f17ad8f3869de26225f7d7848bfce550411c6
Melvin Welton <nivlem65@gmail.com>
2021-12-19 16:44:10 -0600

    updating database and seed sql

--- server.js
+++ server.js
@@ -17,30 +17,12 @@ app.use(express.urlencoded({extended:false}))
 // 
 app.use(require('./controllers'))
 
-//cookies
-// const session = require('express-session');
-// const sequelize = require('./Config/connection');
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// const sess = {
-//     secret: 'Super secret secret',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// };
-// app.use(session(sess));
-
-
-
-
-
-
-//make the server listen
 app.listen(3001,() =>{
     console.log('hello govnar!')
 }); 
 
+app.post('/api/social distance',(req, res)=>{
+    console.log(req.body);
+    res.json(req.body);
+})
