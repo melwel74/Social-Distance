e20b4661a6a5fc834380259c8d6eab2d2d1eb662
dianepj12@gmail.com <dianepj12@gmail.com>
2021-12-19 22:25:28 -0600

    final?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
-var db =openDatabase("social","1.0","itemDB,65535")
+var db =openDatabase("socialdistance","1.0","socialdistance,65535")
 let blogId = decodeURI(location.pathname.split("/").pop());
 
-let docRef = db.collection("blogs").doc(blogId);
 
 docRef.get().then((doc) => {
     if(doc.exists){
@@ -16,8 +15,6 @@ const setupBlog = (data) => {
     const blogTitle = document.querySelector('.title');
     const titleTag = document.querySelector('title');
     const publish = document.querySelector('.published');
-    
-    banner.style.backgroundImage = `url(${data.bannerImage})`;
 
     titleTag.innerHTML += blogTitle.innerHTML = data.title;
     publish.innerHTML += data.publishedAt;
@@ -42,22 +39,6 @@ const addArticle = (ele, data) => {
             let tag = `h${hCount}`;
             ele.innerHTML += `<${tag}>${item.slice(hCount, item.length)}</${tag}>`
         } 
-        //checking for image format
-        else if(item[0] == "!" && item[1] == "["){
-            let seperator;
-
-            for(let i = 0; i <= item.length; i++){
-                if(item[i] == "]" && item[i + 1] == "(" && item[item.length - 1] == ")"){
-                    seperator = i;
-                }
-            }
-
-            let alt = item.slice(2, seperator);
-            let src = item.slice(seperator + 2, item.length - 1);
-            ele.innerHTML += `
-            <img src="${src}" alt="${alt}" class="article-image">
-            `;
-        }
 
         else{
             ele.innerHTML += `<p>${item}</p>`;
@@ -105,7 +86,7 @@ publishBtn.addEventListener('click', () => {
         })
     }
 })
-//end of editor section
+
 const blogSection = document.querySelector('.blogs-section');
 
 // blog section
@@ -122,7 +103,7 @@ const createBlog = (blog) => {
     blogSection.innerHTML += `
     <div class="blog-card">
         <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
-        <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
+        <p class="words">${data.article.substring(0, 200) + '...'}</p>
         <a href="/${blog.id}" class="btn dark">read</a>
     </div>
     `;
--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const path = require('path');
 // const fileupload = require('express-fileupload');
 // added Fs-D
 let initial_path = path.join(__dirname,"public");
-// const exphbs = require('express-handlebars');
-// const hbs = exphbs.create({});
 
+const PORT = process.env.PORT || 3001;
 //Instantiate the server
 const app = express();
 // added Fs-D
@@ -18,37 +17,13 @@ app.get('/',(req,res) =>{
 app.get('/Editor',(req,res)=>{
     res.sendFile(path.join(initial_path,"editor.html"));
 })
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-// app.use(express.static(path.join (__dirname,'public')))
-// app.use(express.json ())
-// app.use(express.urlencoded({extended:false}))
 
 //new express route
 app.get('/api/social-distance', (req,res) =>{
     res.json('social-distance');
 })
 app.use(require('./controllers'))
-//upload link
-// app.post('upload',(req,res)=>{
-//     let file = req.file.image;
-//     let date = new Date();
-//     //image name
-//     let imagename = date.getDate() + date.getTime()+ file.name;
-//     //image upload path
-//     let path = 'public/uploads/' +imagename;
 
-//     //create upload
-//     file.mv(path, (err, result)=>{
-//         if(err){
-//             throw err;
-//         }else{
-//             //our image upload path
-//             res.json('uploads/${imagename}')
-//         }
-//     })
-
-// })
 //make the server listen
 app.listen('3000', () => {
     console.log('listening....')
